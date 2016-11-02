cac97f2e6e739963138bca8dbbe487ccba694338
crearo <bhardwaj.rish@gmail.com>
2016-11-03 01:42:05 +0530

    Step 3 : Created GCE Module and linked Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,21 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-import com.example.Joker;
 import com.example.jokermakerandroid.JokeConstants;
 import com.example.jokermakerandroid.JokesActivity;
+import com.example.rish.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -39,9 +44,41 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
+        /*
         Intent jokeActivityIntent = new Intent(MainActivity.this, JokesActivity.class);
         jokeActivityIntent.putExtra(JokeConstants.EXTRAS_JOKE, Joker.getJoke());
         startActivity(jokeActivityIntent);
+        */
+        new GetJokeAsyncTask().execute();
+    }
+
+    class GetJokeAsyncTask extends AsyncTask<Void, Void, String> {
+        private final String TAG = GetJokeAsyncTask.class.getSimpleName();
+        private MyApi myApiService = null;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            if (myApiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://builditbigger-nanodegree.appspot.com/_ah/api/");
+
+                myApiService = builder.build();
+            }
+
+            try {
+                Log.d(TAG, "Attempting to connect to URL");
+                return myApiService.getJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(MainActivity.this, JokesActivity.class);
+            intent.putExtra(JokeConstants.EXTRAS_JOKE, result);
+            startActivity(intent);
+        }
     }
 
 }
