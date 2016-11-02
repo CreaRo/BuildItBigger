660b0e11615f2b895691d8ee49733fc3993d6097
crearo <bhardwaj.rish@gmail.com>
2016-11-02 16:53:11 +0530

    Step 2 : App button calls Android Lib

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.Joker;
+import com.example.jokermakerandroid.JokeConstants;
+import com.example.jokermakerandroid.JokesActivity;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -36,7 +39,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, Joker.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent jokeActivityIntent = new Intent(MainActivity.this, JokesActivity.class);
+        jokeActivityIntent.putExtra(JokeConstants.EXTRAS_JOKE, Joker.getJoke());
+        startActivity(jokeActivityIntent);
     }
 
 }
